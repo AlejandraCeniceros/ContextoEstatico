2bfcf936902579e04efffa7eb8afc6a3aa876cc0
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-30 08:43:59 -0700

    Agregar archivos de proyecto.

---
+++ ContextoEstatico/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextoEstatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double sumando1 = 10;
+            double sumando2 = 42;
+            double resultado = Matematicas.sumar(sumando1, sumando2);
+
+            Console.WriteLine(sumando1.ToString() + "+" + sumando2.ToString() + "=" + resultado.ToString());
+
+            Console.WriteLine("Pi:" + Matematicas.PI);
+            double radio = 5;
+            Console.WriteLine("Radio"  + radio.ToString() + ", Area" + + Matematicas.areaCirculo(radio));
+
+            Console.Read();
+
+
+        }
+    }
+}
